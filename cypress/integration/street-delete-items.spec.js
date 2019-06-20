6ca45c74f64d10ca13f2af6b43fb921da5a94ca0
Whitman Schorn <wschorn@quartethealth.com>
2019-06-19 20:12:33 -0400

    put back broken delete spec

--- cypress/integration/street-delete-items.spec.js
+++ cypress/integration/street-delete-items.spec.js
@@ -28,23 +28,23 @@ context('User Deleting Items on a Street', () => {
   beforeEach(() => {
     cy.reload()
   })
-  // it('lets users delete an item', () => {
-  //   cy.get('.welcome-panel', { timeout: 10000 }).contains('Welcome to Streetmix')
-  //   cy.get('.welcome-panel>button.close').click()
+  it('lets users delete an item', () => {
+    cy.get('.welcome-panel', { timeout: 10000 }).contains('Welcome to Streetmix')
+    cy.get('.welcome-panel>button.close').click()
 
-  //   // let's delete a thing
-  //   cy.get('.segment-canvas-container').first().trigger('mouseover')
-  //   cy.get('button').contains('Remove').first().click()
-  //   cy.contains('The segment has been removed').should('be.visible')
-  // })
-  // it('lets users delete all items', () => {
-  //   cy.get('.welcome-panel', { timeout: 10000 }).contains('Welcome to Streetmix')
-  //   cy.get('.welcome-panel>button.close').click()
+    // let's delete a thing
+    cy.get('.segment-canvas-container').first().trigger('mouseover')
+    cy.get('button').contains('Remove').first().click()
+    cy.contains('The segment has been removed').should('be.visible')
+  })
+  it('lets users delete all items', () => {
+    cy.get('.welcome-panel', { timeout: 10000 }).contains('Welcome to Streetmix')
+    cy.get('.welcome-panel>button.close').click()
 
-  //   // let's delete all the things
-  //   cy.get('.segment-canvas-container').first().trigger('mouseover')
-  //   cy.get('body').type('{shift}', { release: false })
-  //   cy.get('button').contains('Remove').first().click()
-  //   cy.contains('All segments have been removed.').should('be.visible')
-  // })
+    // let's delete all the things
+    cy.get('.segment-canvas-container').first().trigger('mouseover')
+    cy.get('body').type('{shift}', { release: false })
+    cy.get('button').contains('Remove').first().click()
+    cy.contains('All segments have been removed.').should('be.visible')
+  })
 })
